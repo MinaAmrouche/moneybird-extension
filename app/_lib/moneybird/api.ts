9c86fc41a784c8e030cadf6344ecda4fb21136b8
Mina Amrouche <mina.amrouche9@gmail.com>
2024-03-07 14:14:39 +0100

    Refactor api.ts and handle errors properly

--- app/_lib/moneybird/api.ts
+++ app/_lib/moneybird/api.ts
@@ -1,44 +1,4 @@
-import { unstable_noStore as noStore } from "next/cache";
 import { getSession } from "@/app/_lib/session";
-import { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
-
-const fetchMoneyirdData = async (
-  url: string,
-  accessToken: string,
-  administrationId: string,
-  method: "GET" | "POST" | "PATCH" | "DELETE" = "GET",
-  body: Object | null = null,
-  additionalHeaders: Record<string, string> = {}
-) => {
-  noStore();
-
-  const options: {
-    method: "GET" | "POST" | "PATCH" | "DELETE";
-    headers: Record<string, string>;
-    body?: string;
-  } = {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-      ...additionalHeaders,
-      Authorization: "Bearer " + accessToken,
-    },
-  };
-
-  if (body) {
-    options.body = JSON.stringify(body);
-  }
-
-  try {
-    const fullUrl = `${process.env.API_URL}/${administrationId}/` + url;
-    const response = await fetch(fullUrl, options);
-    return await response.json();
-  } catch (error) {
-    console.error("Error making API request:", error);
-    throw error;
-  }
-};
 
 export const fetchData = async (
   url: string,
@@ -52,41 +12,31 @@ export const fetchData = async (
     throw new Error("You must be logged in.");
   }
 
-  const data = await fetchMoneyirdData(
-    url,
-    session.accessToken,
-    session.user.administrationId,
+  const options: {
+    method: "GET" | "POST" | "PATCH" | "DELETE";
+    headers: Record<string, string>;
+    body?: string;
+  } = {
     method,
-    body,
-    additionalHeaders
-  );
-  return data;
-};
+    headers: {
+      "Content-Type": "application/json",
+      ...additionalHeaders,
+      Authorization: "Bearer " + session.accessToken,
+    },
+  };
 
-export const fetchApiData = async (
-  req: NextRequest,
-  url: string,
-  method: "GET" | "POST" | "PATCH" | "DELETE" = "GET",
-  body: Object | null = null,
-  additionalHeaders: Record<string, string> = {}
-) => {
-  const token = await getToken({ req });
-
-  if (!token) {
-    return Response.redirect("/api/auth/signin");
+  if (body) {
+    options.body = JSON.stringify(body);
   }
 
-  const session = await getSession();
-  if (!session) {
-    throw new Error("You must be logged in.");
+  const fullUrl =
+    `${process.env.API_URL}/${session.user.administrationId}/` + url;
+  const response = await fetch(fullUrl, options);
+
+  if (response.statusText !== "OK") {
+    const error = await response.json();
+    throw new Error(error);
   }
 
-  return fetchMoneyirdData(
-    url,
-    token.accessToken,
-    session.user.administrationId,
-    method,
-    body,
-    additionalHeaders
-  );
+  return await response.json();
 };
