8f219f040483236eb7a9827e0688766bee3ccac1
Mina Amrouche <mina.amrouche9@gmail.com>
2024-03-07 14:32:57 +0100

    Implement pagination

--- app/(dashboard)/_components/table.tsx
+++ app/(dashboard)/_components/table.tsx
@@ -1,122 +1,101 @@
-import { Row, ProjectProductMap, State, Period } from "@/app/_lib/definitions";
-import { Product, TimeEntry } from "@/app/_lib/moneybird/definitions";
-import { Project } from "@prisma/client";
-import { formatTime, projectsToMap } from "@/app/_lib/utils";
+"use client";
+
+import { Row } from "@/app/_lib/definitions";
+import { formatTime } from "@/app/_lib/utils";
 import clsx from "clsx";
-import { getSession } from "@/app/_lib/session";
-import { db } from "@/app/_lib/db";
-import { fetchData } from "@/app/_lib/moneybird/api";
-import { createRows } from "@/app/(dashboard)/utils";
+import Pagination from "@/app/_components/pagination";
+import { useEffect, useState } from "react";
 
-const getProjects = async () => {
-  const session = await getSession();
-  if (session) {
-    const user = session.user;
-    return db.project.findMany({
-      where: {
-        userId: user.id,
-      },
-    });
-  }
-
-  return [];
-};
-
-const TimeEntriesTable = async ({
-  state,
-  period,
+const Table = ({
+  rows,
+  totalAmount,
+  totalTime,
+  hasNextPage,
+  page,
 }: {
-  state: State;
-  period: Period;
+  rows: Row[];
+  totalAmount: number;
+  totalTime: number;
+  hasNextPage: boolean;
+  page: number;
 }) => {
-  const timeEntriesPromise = fetchData(
-    `time_entries?filter=${encodeURIComponent(
-      `state:${state === "billed" ? "all" : state},period:${period}`
-    )}`
-  );
-  const productsPromise = fetchData("products");
+  const [totalPages, setTotalPages] = useState<number>(page);
 
-  let [timeEntries, products, projects]: [TimeEntry[], Product[], Project[]] =
-    await Promise.all([timeEntriesPromise, productsPromise, getProjects()]);
-
-  if (state === "billed") {
-    timeEntries = timeEntries.filter(
-      (timeEntry) => timeEntry.billable && timeEntry.detail
-    );
-  }
-
-  const projectProductMap: ProjectProductMap = projectsToMap(projects);
-
-  const [rows, totalAmount, totalTime]: [Row[], number, number] = createRows(
-    timeEntries,
-    products,
-    projectProductMap
-  );
+  useEffect(() => {
+    if (hasNextPage && page >= totalPages) {
+      setTotalPages(page + 1);
+    } else if (!hasNextPage || totalPages < page || !rows.length) {
+      setTotalPages(page);
+    }
+  }, [page, hasNextPage, rows]);
 
   return (
-    <div className="flex border rounded-lg p-4 dark:p-0 bg-white dark:bg-slate-900 mt-4">
-      <table className="table-auto w-full">
-        <thead className="font-semibold uppercase text-slate-600 dark:text-slate-400 text-sm">
-          <tr className="border-b-2">
-            <td className="p-2">Date</td>
-            <td className="p-2">Description</td>
-            <td className="p-2">Project</td>
-            <td className="p-2">Contact</td>
-            <td className="p-2">State</td>
-            <td className="p-2">Time</td>
-            <td className="p-2">Amount</td>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row) => (
-            <tr
-              key={row.id}
-              className="border-b even:bg-blue-50/50 dark:even:bg-slate-800/50 dark:odd:bg-slate-800/50"
-            >
-              <td className="p-2">{row.date}</td>
-              <td className="p-2">{row.description}</td>
-              <td className="p-2">{row.project}</td>
-              <td className="p-2">{row.contact}</td>
-              <td className="p-2">
-                <div
-                  className={clsx(
-                    "relative grid select-none items-center whitespace-nowrap rounded-full py-1.5 px-3 w-fit font-sans text-xs font-bold uppercase",
-                    {
-                      "bg-cyan-500 dark:bg-transparent  dark:border dark:border-cyan-500 text-white dark:text-cyan-500":
-                        row.state === "Open",
-                      "bg-indigo-500 dark:bg-transparent dark:border dark:border-indigo-500 text-white dark:text-indigo-500":
-                        row.state === "Billed",
-                      "bg-gray-900 dark:bg-transparent dark:border dark:border-gray-300 text-gray-900 dark:text-gray-300":
-                        row.state === "Non-billable",
-                    }
-                  )}
-                >
-                  <span>{row.state}</span>
-                </div>
-              </td>
-              <td className="p-2">{row.time}</td>
-              <td className="p-2">€{row.amount}</td>
+    <>
+      <div className="flex border rounded-lg p-4 dark:p-0 bg-white dark:bg-slate-900 mt-4">
+        <table className="table-auto w-full">
+          <thead className="font-semibold uppercase text-slate-600 dark:text-slate-400 text-sm">
+            <tr className="border-b-2">
+              <td className="p-2">Date</td>
+              <td className="p-2">Description</td>
+              <td className="p-2">Project</td>
+              <td className="p-2">Contact</td>
+              <td className="p-2">State</td>
+              <td className="p-2">Time</td>
+              <td className="p-2">Amount</td>
             </tr>
-          ))}
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td className="p-2">
-              <b>Total</b>
-            </td>
-            <td className="p-2">
-              <b>{formatTime(totalTime)}</b>
-            </td>
-            <td className="p-2">
-              <b>€{totalAmount.toFixed(2)}</b>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {rows.map((row) => (
+              <tr
+                key={row.id}
+                className="border-b even:bg-blue-50/50 dark:even:bg-slate-800/50 dark:odd:bg-slate-800/50"
+              >
+                <td className="p-2">{row.date}</td>
+                <td className="p-2">{row.description}</td>
+                <td className="p-2">{row.project}</td>
+                <td className="p-2">{row.contact}</td>
+                <td className="p-2">
+                  <div
+                    className={clsx(
+                      "relative grid select-none items-center whitespace-nowrap rounded-full py-1.5 px-3 w-fit font-sans text-xs font-bold uppercase",
+                      {
+                        "bg-cyan-500 dark:bg-transparent  dark:border dark:border-cyan-500 text-white dark:text-cyan-500":
+                          row.state === "Open",
+                        "bg-indigo-500 dark:bg-transparent dark:border dark:border-indigo-500 text-white dark:text-indigo-500":
+                          row.state === "Billed",
+                        "bg-gray-900 dark:bg-transparent dark:border dark:border-gray-300 text-gray-900 dark:text-gray-300":
+                          row.state === "Non-billable",
+                      }
+                    )}
+                  >
+                    <span>{row.state}</span>
+                  </div>
+                </td>
+                <td className="p-2">{row.time}</td>
+                <td className="p-2">€{row.amount}</td>
+              </tr>
+            ))}
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className="p-2">
+                <b>Total</b>
+              </td>
+              <td className="p-2">
+                <b>{formatTime(totalTime)}</b>
+              </td>
+              <td className="p-2">
+                <b>€{totalAmount.toFixed(2)}</b>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <Pagination totalPages={totalPages} />
+    </>
   );
 };
 
-export default TimeEntriesTable;
+export default Table;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
 import Filters from "@/app/(dashboard)/_components/filters";
 import Spinner from "@/app/_components/spinner";
-import TimeEntriesTable from "@/app/(dashboard)/_components/table";
+import TimeEntriesTable from "@/app/(dashboard)/_components/timeEntriesTable";
 import Title from "@/app/_components/title";
 import Subtitle from "@/app/_components/subtitle";
-import { Period, State } from "../_lib/definitions";
+import { Period, State } from "@/app/_lib/definitions";
 
 export default async function Home({
   searchParams,
@@ -12,10 +12,12 @@ export default async function Home({
   searchParams?: {
     state?: string;
     period?: string;
+    page?: number;
   };
 }) {
   const state = searchParams?.state || "all";
   const period = searchParams?.period || "this_month";
+  const page = searchParams?.page;
 
   return (
     <>
@@ -23,7 +25,11 @@ export default async function Home({
       <Subtitle>Total hours and amount</Subtitle>
       <Filters state={state as State} period={period as Period} />
       <Suspense fallback={<Spinner />}>
-        <TimeEntriesTable state={state as State} period={period as Period} />
+        <TimeEntriesTable
+          state={state as State}
+          period={period as Period}
+          page={Number(page)}
+        />
       </Suspense>
     </>
   );
