2b338ae440ada4ec1d057cbe26a158dbfbce9799
Mina Amrouche <mina.amrouche9@gmail.com>
2024-03-26 14:32:11 +0100

    Add vercel analytics

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -24,7 +24,7 @@ export default async function Navbar() {
   const user = session?.user || null;
 
   return (
-    <nav className="block fixed top-0 w-full h-18 z-10 px-10 py-3 mx-auto bg-white dark:bg-slate-950 text-gray-900 dark:text-white shadow-md bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200">
+    <nav className="block fixed top-0 w-full h-16 z-10 px-10 py-3 mx-auto bg-white dark:bg-slate-950 text-gray-900 dark:text-white shadow-md bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200">
       <div className="flex items-center justify-between">
         <Link
           href="/"
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import Navbar from "@/app/_components/navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="flex min-h-screen flex-col p-10 pt-20 antialiased bg-white dark:bg-slate-900 text-gray-900 dark:text-white">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
